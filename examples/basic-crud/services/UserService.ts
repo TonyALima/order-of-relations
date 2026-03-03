4dc9c61593adc8b915f94a5401c0d7b85aa4ded0
Tony Albert Lima <80111558+TonyALima@users.noreply.github.com>
2026-03-02 22:25:12 -0300

    feat: implement update method in Repository class and rename save method to create.

--- examples/basic-crud/services/UserService.ts
+++ examples/basic-crud/services/UserService.ts
@@ -7,7 +7,7 @@ export class UserService {
   private userRepository!: Repository<User>;
 
   async createUser(name: string, email: string) {
-    return this.userRepository.save({ name, email });
+    return this.userRepository.create({ name, email });
   }
 
   async listUsers() {
--- src/core/repository.ts
+++ src/core/repository.ts
@@ -25,7 +25,7 @@ export class Repository<T, PK extends keyof T = 'id' extends keyof T ? 'id' : ne
     return rows;
   }
 
-  async save(entity: Omit<T, PK>): Promise<T[PK]> {
+  async create(entity: Omit<T, PK>): Promise<T[PK]> {
     const meta = metadataStorage.get(this.entity)!;
     const sql = Database.getConnection();
 
@@ -46,5 +46,29 @@ export class Repository<T, PK extends keyof T = 'id' extends keyof T ? 'id' : ne
       RETURNING ${sql(primaryColumn.columnName)}
       `;
     return result[0]![primaryColumn.columnName]!;
+  }
+
+  async update(entity: T): Promise<void> {
+    const meta = metadataStorage.get(this.entity)!;
+    const sql = Database.getConnection();
+
+    const primaryColumn = meta.columns.find((c) => c.primary)!;
+    const columns = meta.columns.filter((c) => !c.primary);
+    const tableName = sql(meta.tableName);
+
+    const objectToUpdate: Record<string, unknown> = {};
+    columns.forEach((col) => {
+      const columnName = col.columnName;
+      const propertyName = col.propertyName as keyof T;
+      objectToUpdate[columnName] = entity[propertyName];
+    });
+
+    const primaryKeyValue = entity[primaryColumn.propertyName as PK];
+
+    await sql`
+      UPDATE ${tableName}
+      SET ${sql(objectToUpdate)}
+      WHERE ${sql(primaryColumn.columnName)} = ${primaryKeyValue}
+    `;
   }
 }
