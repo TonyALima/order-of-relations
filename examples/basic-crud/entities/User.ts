c283e99cd9b460b4ddfac853e499a067664c845b
Tony Albert Lima <80111558+TonyALima@users.noreply.github.com>
2026-03-12 19:14:44 -0300

    refactor: update Entity decorator to allow optional table name mapping

--- examples/basic-crud/entities/User.ts
+++ examples/basic-crud/entities/User.ts
@@ -1,7 +1,7 @@
 import { Entity, Column, PrimaryColumn } from '../../../src';
 import { COLUMN_TYPE } from '../../../src/core/sql-types';
 
-@Entity('user')
+@Entity()
 export class User {
   @PrimaryColumn({ type: COLUMN_TYPE.SERIAL })
   id!: number;
--- src/decorators/entity.ts
+++ src/decorators/entity.ts
@@ -5,11 +5,13 @@ const RELATIONS_KEY = Symbol('relations');
 
 export { COLUMNS_KEY, RELATIONS_KEY };
 
-export function Entity(tableName: string) {
+export function Entity(mapTableName?: string) {
   return function <T extends abstract new (...args: unknown[]) => unknown>(
     value: T,
     context: ClassDecoratorContext<T>,
   ) {
+    const tableName = mapTableName ?? String(context.name);
+
     const columns = (context.metadata[COLUMNS_KEY] as ColumnMetadata[]) ?? [];
     const relations = (context.metadata[RELATIONS_KEY] as RelationMetadata[]) ?? [];
     metadataStorage.set(value, { tableName, columns, relations });
