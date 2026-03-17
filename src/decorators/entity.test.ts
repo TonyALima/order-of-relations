39f26567bc64c358ff4d0cbb413673e060e27cc9
Tony Albert Lima <80111558+TonyALima@users.noreply.github.com>
2026-03-17 17:23:42 -0300

    test: add describe grouping to entity.test.ts

--- src/decorators/entity.test.ts
+++ src/decorators/entity.test.ts
@@ -1,35 +1,37 @@
-import { expect, test } from 'bun:test';
+import { expect, test, describe } from 'bun:test';
 import { Database } from '../core/database';
 import { COLUMN_TYPE } from '../core/sql-types';
 import { Column, PrimaryColumn } from './column';
 import { Entity } from './entity';
 
-test('Entity stores table, column, and relation metadata for the decorated class', () => {
-  @Entity('user')
-  class User {
-    @PrimaryColumn({ type: COLUMN_TYPE.SERIAL })
-    id!: number;
+describe('@Entity / @Column decorators', () => {
+  test('stores table, column, and relation metadata for the decorated class', () => {
+    @Entity('user')
+    class User {
+      @PrimaryColumn({ type: COLUMN_TYPE.SERIAL })
+      id!: number;
 
-    @Column({ type: COLUMN_TYPE.TEXT })
-    name!: string;
-  }
+      @Column({ type: COLUMN_TYPE.TEXT })
+      name!: string;
+    }
 
-  expect(Database.getInstance().getMetadata().get(User)).toEqual({
-    tableName: 'user',
-    columns: [
-      {
-        propertyName: 'id',
-        columnName: 'id',
-        type: COLUMN_TYPE.SERIAL,
-        primary: true,
-      },
-      {
-        propertyName: 'name',
-        columnName: 'name',
-        type: COLUMN_TYPE.TEXT,
-        primary: undefined,
-      },
-    ],
-    relations: [],
+    expect(Database.getInstance().getMetadata().get(User)).toEqual({
+      tableName: 'user',
+      columns: [
+        {
+          propertyName: 'id',
+          columnName: 'id',
+          type: COLUMN_TYPE.SERIAL,
+          primary: true,
+        },
+        {
+          propertyName: 'name',
+          columnName: 'name',
+          type: COLUMN_TYPE.TEXT,
+          primary: undefined,
+        },
+      ],
+      relations: [],
+    });
   });
 });
